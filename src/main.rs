781e0ed821dbd3535e3d6635f4d977a138edc1be
Ohsey <admin@proton.me>
2025-12-07 13:39:11 +0000

    Start work on swapchain and surface work. Update winit to newest version and rewrite main.rs to fit the new way of doing things in winit.

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,70 @@ mod content;
 mod rendering;
 
 use winit::{
-    event::{Event, WindowEvent},
+    event::{WindowEvent},
     event_loop::EventLoop,
     window::Window
 };
+use winit::application::ApplicationHandler;
+use winit::event::{DeviceEvent, DeviceId, StartCause};
+use winit::event_loop::{ActiveEventLoop, ControlFlow};
+use winit::window::{WindowAttributes, WindowId};
 
-async fn run(event_loop: EventLoop<()>, window: &Window) {
-    event_loop.run(move |event, target| {
-        if let Event::WindowEvent { window_id: _, event } = event {
-            match event {
-                WindowEvent::Resized(new_size) => {
-                    todo!("Not implemented yet");
-                }
-                WindowEvent::RedrawRequested => {
-                    todo!("Not implemented yet");
-                }
-                WindowEvent::CloseRequested => {
-                    target.exit();
-                }
-                _ => {}
-            }
-        }
-    }).unwrap();
+struct App {
+    device: rendering::Device,
+    view: Option<rendering::WindowView>,
+}
+
+impl App {
+    fn new(event_loop: &EventLoop<()>) -> Self {
+        let device = rendering::Device::new(event_loop, vec![]).expect("Failed to setup Vulkan - Aborting!");
+        Self { device, view: None }
+    }
+}
+
+impl ApplicationHandler for App {
+    fn new_events(&mut self, event_loop: &ActiveEventLoop, cause: StartCause) {
+        todo!()
+    }
+
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if let Some(_) = &self.view { return; }
+        let view = rendering::WindowView::new(event_loop, &mut self.device).expect("Failed to setup Vulkan - Aborting!");
+        self.view = Some(view);
+    }
+
+    fn user_event(&mut self, event_loop: &ActiveEventLoop, event: ()) {
+        todo!()
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
+        todo!()
+    }
+
+    fn device_event(&mut self, event_loop: &ActiveEventLoop, device_id: DeviceId, event: DeviceEvent) {
+        todo!()
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        todo!()
+    }
+
+    fn suspended(&mut self, event_loop: &ActiveEventLoop) {
+        todo!()
+    }
+
+    fn exiting(&mut self, event_loop: &ActiveEventLoop) {
+        todo!()
+    }
+
+    fn memory_warning(&mut self, event_loop: &ActiveEventLoop) {
+        todo!()
+    }
 }
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let window = winit::window::WindowBuilder::new().with_title("RL Replay Viewer").build(&event_loop).unwrap();
-    let device = crate::rendering::Device::new().expect("Unable to initialize vulkan!");
-    pollster::block_on(run(event_loop, &window));
+    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut app = App::new(&event_loop);
+    event_loop.run_app(&mut app).expect("App crashed!");
 }
--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,5 +1,12 @@
+use std::ffi::{CString, NulError};
+use std::os::raw::c_char;
 use ash::{vk, Instance};
-use ash::vk::PhysicalDevice;
+use ash::khr::swapchain;
+use ash::vk::{PhysicalDevice, PhysicalDeviceType, SwapchainCreateInfoKHR, SwapchainKHR};
+use winit::error::OsError;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::raw_window_handle::{HandleError, HasDisplayHandle, HasWindowHandle};
+use winit::window::{Window, WindowAttributes};
 
 #[derive(Debug)]
 pub enum VulkanError {
@@ -7,6 +14,9 @@ pub enum VulkanError {
     Error(vk::Result),
     NoSuitableDevice,
     UnableToFindQueueFamily,
+    NulError(NulError),
+    WindowHandleError(HandleError),
+    OsError(OsError),
 }
 
 impl From<ash::LoadingError> for VulkanError {
@@ -21,16 +31,56 @@ impl From<vk::Result> for VulkanError {
     }
 }
 
+impl From<NulError> for VulkanError {
+    fn from(err: NulError) -> Self {
+        Self::NulError(err)
+    }
+}
+
+impl From<HandleError> for VulkanError {
+    fn from(err: HandleError) -> Self {
+        Self::WindowHandleError(err)
+    }
+}
+
+impl From<OsError> for VulkanError {
+    fn from(err: OsError) -> Self {
+        Self::OsError(err)
+    }
+}
+
+pub struct WindowView {
+    window: Window,
+    surface: vk::SurfaceKHR,
+    swapchain: SwapchainKHR,
+}
+
 pub struct Device {
     entry: ash::Entry,
-    instance: ash::Instance,
+    instance: Instance,
     logical: ash::Device,
-    physical: vk::PhysicalDevice,
+    physical: PhysicalDevice,
+    swapchain_loader: swapchain::Device,
+}
+
+impl WindowView {
+    pub fn new(event_loop: &ActiveEventLoop, device: &Device) -> Result<Self, VulkanError> {
+        let attribs = WindowAttributes::default().with_title("RL Replay Viewer");
+        let window = event_loop.create_window(attribs)?;
+        let (surface, swapchain) = device.link_to_window(&window)?;
+        Ok(Self { window, surface, swapchain })
+    }
 }
 
 impl Device {
-    pub fn new() -> Result<Self, VulkanError> {
-        let (entry, instance) = unsafe { Self::init()? };
+    pub fn new(event_loop: &EventLoop<()>, extensions: Vec<&str>) -> Result<Self, VulkanError> {
+        // Need this to prevent use after free later on when using as a *const c_char
+        let extensions_cstr = extensions.iter().map(|&s| CString::new(s).unwrap()).collect::<Vec<_>>();
+        let mut extensions_raw = extensions_cstr.iter().map(|s| s.as_ptr()).collect::<Vec<_>>();
+        extensions_raw.append(&mut ash_window::enumerate_required_extensions(event_loop.display_handle()?.as_raw())?
+            .iter().map(|&cs| cs).collect::<Vec<_>>());
+        extensions_raw.push(swapchain::NAME.as_ptr());
+        let (entry, instance) = unsafe { Self::init(&extensions_raw)? };
         let physical = unsafe { Self::pick_physical(&instance)? };
         let queue_family_index = if let Some(u) = unsafe { Self::find_queue_family_index(&instance, physical) } {
             u
@@ -38,12 +88,12 @@ impl Device {
             return Err(VulkanError::UnableToFindQueueFamily);
         };
         let logical = unsafe { Self::create_logical(&instance, physical, queue_family_index)? };
-        Ok(Self { entry, instance, logical, physical })
+        let swapchain_loader = swapchain::Device::new(&instance, &logical);
+        Ok(Self { entry, instance, logical, physical, swapchain_loader })
     }
 
     #[inline]
-    unsafe fn pick_physical(instance: &ash::Instance) -> Result<vk::PhysicalDevice, VulkanError> {
-        use vk::{PhysicalDevice, PhysicalDeviceType};
+    unsafe fn pick_physical(instance: &Instance) -> Result<PhysicalDevice, VulkanError> {
         let mut selected = None;
         let all = instance.enumerate_physical_devices()?;
         for physical_device in all {
@@ -63,19 +113,19 @@ impl Device {
     }
 
     #[inline]
-    unsafe fn pick_between(instance: &ash::Instance, properties: &vk::PhysicalDeviceProperties, this: vk::PhysicalDevice, other: Option<vk::PhysicalDevice>) -> Option<vk::PhysicalDevice> {
+    unsafe fn pick_between(instance: &Instance, properties: &vk::PhysicalDeviceProperties, this: PhysicalDevice, other: Option<PhysicalDevice>) -> Option<PhysicalDevice> {
         let dev = if let Some(dev) = other {
             dev
         } else {
             return Some(this);
         };
         let properties_o = instance.get_physical_device_properties(dev);
-        let d = if properties.device_type == vk::PhysicalDeviceType::INTEGRATED_GPU &&
-            properties_o.device_type == vk::PhysicalDeviceType::VIRTUAL_GPU {
+        let d = if properties.device_type == PhysicalDeviceType::INTEGRATED_GPU &&
+            properties_o.device_type == PhysicalDeviceType::VIRTUAL_GPU {
             this
-        } else if properties.device_type == vk::PhysicalDeviceType::DISCRETE_GPU &&
-            (properties_o.device_type == vk::PhysicalDeviceType::VIRTUAL_GPU ||
-                properties_o.device_type == vk::PhysicalDeviceType::INTEGRATED_GPU)  {
+        } else if properties.device_type == PhysicalDeviceType::DISCRETE_GPU &&
+            (properties_o.device_type == PhysicalDeviceType::VIRTUAL_GPU ||
+                properties_o.device_type == PhysicalDeviceType::INTEGRATED_GPU)  {
             this
         } else {
             dev
@@ -83,22 +133,18 @@ impl Device {
         Some(d)
     }
     #[inline]
-    unsafe fn init() -> Result<(ash::Entry, ash::Instance), VulkanError> {
-        let app_info = vk::ApplicationInfo {
-            api_version: vk::make_api_version(0, 1, 0, 0),
-            ..Default::default()
-        };
-        let create_info = vk::InstanceCreateInfo {
-            p_application_info: &app_info,
-            ..Default::default()
-        };
+    unsafe fn init(extensions: &[*const c_char]) -> Result<(ash::Entry, Instance), VulkanError> {
         let entry = ash::Entry::load()?;
+        let app_info = vk::ApplicationInfo::default().api_version(vk::API_VERSION_1_0);
+        let create_info = vk::InstanceCreateInfo::default()
+            .application_info(&app_info)
+            .enabled_extension_names(extensions);
         let instance = entry.create_instance(&create_info, None)?;
         Ok((entry, instance))
     }
 
     #[inline]
-    unsafe fn create_logical(instance: &Instance, physical: vk::PhysicalDevice, queue_family_index: u32) -> Result<ash::Device, VulkanError> {
+    unsafe fn create_logical(instance: &Instance, physical: PhysicalDevice, queue_family_index: u32) -> Result<ash::Device, VulkanError> {
         let features = vk::PhysicalDeviceFeatures::default();
         let queue_info = vk::DeviceQueueCreateInfo::default().queue_family_index(queue_family_index);
         let create_info = vk::DeviceCreateInfo::default()
@@ -118,6 +164,20 @@ impl Device {
             index += 1;
         }
         None
+    }
+
+    // Surface and swapchain stuff
+
+    pub fn link_to_window(&self, window: &Window) -> Result<(vk::SurfaceKHR, SwapchainKHR), VulkanError> {
+        let surface = unsafe { ash_window::create_surface(&self.entry,
+                                                &self.instance,
+                                                window.display_handle()?.as_raw(),
+                                                window.window_handle()?.as_raw(),
+                                                None)? };
+        // TODO: Handle queue families properly instead of doing it "lazily" and definitely incorrectly.
+        let create_info = SwapchainCreateInfoKHR::default().surface(surface);
+        let swapchain = unsafe { self.swapchain_loader.create_swapchain(&create_info, None)? };
+        Ok((surface, swapchain))
     }
 }
 
